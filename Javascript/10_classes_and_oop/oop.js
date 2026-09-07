77adfe523ac1d4927c7b36a66c1652a15b2c78ff
Santosh <spant671@gmail.com>
2026-09-07 16:56:43 +0530

    oop

---
+++ Javascript/10_classes_and_oop/oop.js
@@ -0,0 +1,15 @@
+const user = {
+    username: 'sans',
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from the databse.");
+        //console.log(`Username: ${this.username}`);
+        console.log(this)
+    } 
+}
+console.log(user.username);
+console.log(user.getUserDetails());
+console.log(this)
+
